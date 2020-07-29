02b400fc1b85db9941c5fa9b915db151abe26bb2
Ann <annshulyndina@gmail.com>
2020-07-29 11:41:31 +0300

    Loop_last

--- loop.js
+++ loop.js
@@ -2,10 +2,10 @@
 
 let num = 50;
 
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// }
+while (num <= 55) {
+    console.log(num);
+    num++;
+}
 
 //сделать цикл и выйти из него
 do {
@@ -23,5 +23,5 @@ for (let i=1; i < 10; i++) {
         // break;
         continue;
     }
-
-    console.log(i); }
+    console.log(i); 
+}
