0761562ef8847c95d2e8431fcc421e3d81aab429
Ann <annshulyndina@gmail.com>
2020-07-31 15:27:33 +0300

    spread_operator

--- objects_destructuring.js
+++ objects_destructuring.js
@@ -15,8 +15,8 @@ const options = {
 options.makeTest();
 
 //метод деструктуризации - вытащить из обекта вложенные объект
-const {border, bg} = options.colors;
-console.log(border);
+// const {border, bg} = options.colors;
+// console.log(border);
 
 // console.log(Object.keys(options).length); // (!) метод получения количество ключей в объекте
 
@@ -25,16 +25,16 @@ console.log(border);
 // delete options.name;
 // console.log(options);
 // let counter = 0;
-// for (let key in options) {
-//     if (typeof(options[key]) === 'object'){
-//         for (let i in options [key]){
-//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`); 
-//не понятно с ${options[key]} - как это равно значению ключа?
-//             counter++;
-//         }
-//         } else {
-//             console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//             counter++; // метод перебора(сколько ключей в объекте) 
-//     }
-// }
-// console.log(counter);
+for (let key in options) {
+    if (typeof(options[key]) === 'object'){
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`); 
+не понятно с ${options[key]} - как это равно значению ключа?
+            counter++;
+        }
+        } else {
+            console.log(`Свойство ${key} имеет значение ${options[key]}`);
+            counter++; // метод перебора(сколько ключей в объекте) 
+    }
+}
+console.log(counter);
