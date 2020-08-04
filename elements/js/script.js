461cabd70d7c09aea63959728dae26d2f14f1665
Ann <annshulyndina@gmail.com>
2020-08-04 15:23:52 +0300

    действия_с_элементами_на странице

--- elements/js/script.js
+++ elements/js/script.js
@@ -1,18 +1,53 @@
 'use strict';
 
-const box = document.getElementById('box');
-console.log(box);
+const box = document.getElementById('box'),
+    btns = document.getElementsByTagName('button'),
+    circles = document.getElementsByClassName('circle'),
+    wrapper = document.querySelector('.wrapper'),
+    hearts = wrapper.querySelectorAll('.heart'), //при работе с селекторами обязательно ставить точку
+    oneHeart = wrapper.querySelector('.heart');
 
-const btns = document.getElementsByTagName('button')[1];
-console.log(btns);
+// console.dir(box);
 
-const circles = document.getElementsByClassName('circle');
-console.log(circles);
 
-const hearts = document.querySelectorAll('.heart'); //при работе с селекторами обязательно ставить точку
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
+//the same
+const num = 500;
+box.style.cssText = `background-color: blue; width: ${num}px`;
+
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
+
+// for (let i = 0; i<hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'blue';
+// }
+//the same
 hearts.forEach(item => {
-console.log(item);
+    item.style.backgroundColor = 'blue';
 });
 
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
+const div = document.createElement('div');
+// const text = document.createTextNode('Тут был я');
+
+div.classList.add('black');
+// document.body.append(div);
+// document.querySelector('.wrapper').append(div);
+wrapper.append(div);
+// wrapper.prepend(div);
+
+// hearts[2].before(div);
+// hearts[0].after(div);
+//circles[0].remove();
+
+hearts[0].replaceWith(circles[0]);
+// wrapper.replaceChild(circles[0], hearts[0]);
+
+div.innerHTML = "<h1>Hello World</h1>"; 
+//div.textContent = "hello"; // только для текста
+div.insertAdjacentHTML("afterend", '<h2>hello</h2>');
+//beforebegin; afterbegin; beforeend; afterend
+
+
+
+
