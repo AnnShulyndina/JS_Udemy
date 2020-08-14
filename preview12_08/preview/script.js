6afad4a0ab4dd0bbe1b182b487169a7c67319546
Ann <annshulyndina@gmail.com>
2020-08-14 12:57:53 +0300

    параметры документа,scroll

--- preview12_08/preview/script.js
+++ preview12_08/preview/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const box = document.querySelector('.box');
+const box = document.querySelector('.box'),
+btn = document.querySelector('button');
 
 const width = box.scrollWidth;
 const height = box.scrollHeight;
@@ -13,3 +14,17 @@ const height = box.scrollHeight;
 
 
 console.log(width, height);
+
+btn.addEventListener('click', ()=> {
+    // box.style.height = box.scrollHeight + 'px';
+    console.log(box.scrollTop);
+});
+
+console.log(box.getBoundingClientRect().top);
+const style  = window.getComputedStyle(box);
+console.log(style.display);
+
+document.documentElement.scrollTop = 0; //можно сделать стрелочку которая
+//будет возвращать страничку к началу
+window.scrollBy(0, 400)
+window.scrollTo(0, 400) //перемещение пользователя по странице
