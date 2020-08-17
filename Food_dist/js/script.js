8d3ee082ac7452d3ece937dd48b389efe70f8c2b
Ann <annshulyndina@gmail.com>
2020-08-17 15:33:55 +0300

    Модальное_окно

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -50,12 +50,13 @@ window.addEventListener('DOMContentLoaded', () => {
     const deadline = '2020-10-11';
 
     function getTimeRemaining(endtime) {
+        //Math.floor() - округляет аргумент до ближайшего меньшего целого
         const t = Date.parse(endtime) - Date.parse(new Date()),
-        days = Math.floor( (t/(1000*60*60*24)) ),
-        seconds = Math.floor( (t/1000) % 60 ),
-        minutes = Math.floor( (t/1000/60) % 60 ),
-        hours = Math.floor( (t/(1000*60*60) % 24) );
-
+            days = Math.floor((t / (1000 * 60 * 60 * 24))),//в одной секунде 1000 миллисекунд
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24));//%24 - делим на общее количество часов в сутках чтобы получить хвостик
+        //возвращаем переменные наружу т.к. данные переменные выше работают только внутри функции
         return {
             'total': t,
             'days': days,
@@ -65,14 +66,16 @@ window.addEventListener('DOMContentLoaded', () => {
         };
     }
 
-    function getZero(num){
-        if(num >= 0 && num < 10) {
-            return`0${num}`;
+    function getZero(num) { //для нолика впереди
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
         } else {
             return num;
         }
     }
+
     function setClock(selector, endtime) {
+        
         const timer = document.querySelector(selector),
             days = timer.querySelector('#days'),
             hours = timer.querySelector('#hours'),
@@ -80,7 +83,7 @@ window.addEventListener('DOMContentLoaded', () => {
             seconds = timer.querySelector('#seconds'),
             timeInterval = setInterval(updateClock, 1000);
 
-            updateClock();
+        updateClock();//для сразу текущей даты, чтобы дата не моргала
 
         function updateClock() {
             const t = getTimeRemaining(endtime);
@@ -90,15 +93,72 @@ window.addEventListener('DOMContentLoaded', () => {
             minutes.innerHTML = getZero(t.minutes);
             seconds.innerHTML = getZero(t.seconds);
 
-            if (t.total <= 0) {
+            if (t.total <= 0) { //если время вышло то таймер не будет обновлятьсяё
                 clearInterval(timeInterval);
             }
 
         }
     }
+    
     setClock('.timer', deadline);
+
+    // модальное окно
+    const modalTrigger = document.querySelectorAll('[data-modal]'), //[] потому что это атрибут
+        modal = document.querySelector('.modal'),
+        modalCloseBtn = document.querySelector('[data-close]');
+
+    modalTrigger.forEach(btn =>{
+        btn.addEventListener('click', ()=> {
+            modal.classList.add('show'); //при клике должно показаться модальное окно
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden'; //при открытии модального окна сама страница сайта не 
+            //прокручивается благодаря overflow
+        });
+    });
+    
+   
+
+    modalCloseBtn.addEventListener('click', ()=> {
+        modal.classList.add('hide'); //при клике должно закрываться модальное окно
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    });
 });
 
-//сделать свой таймер
 
+
+//     const modalTrigger = document.querySelectorAll('[data-modal]'),
+//     modal = document.querySelector('.modal'),
+//     modalCloseBtn = document.querySelector('[data-close]');
+
+// modalTrigger.forEach(btn => {
+//     btn.addEventListener('click', function() {
+//         modal.classList.add('show');
+//         modal.classList.remove('hide');
+//         // Либо вариант с toggle - но тогда назначить класс в верстке
+//         document.body.style.overflow = 'hidden';
+//     });
+// });
+
+// function closeModal() {
+//     modal.classList.add('hide');
+//     modal.classList.remove('show');
+//     // Либо вариант с toggle - но тогда назначить класс в верстке
+//     document.body.style.overflow = '';
+// }
+
+// modalCloseBtn.addEventListener('click', closeModal);
+
+// modal.addEventListener('click', (e) => {
+//     if (e.target === modal) {
+//         closeModal();
+//     }
+// });
+
+// document.addEventListener('keydown', (e) => {
+//     if (e.code === "Escape" && modal.classList.contains('show')) { 
+//         closeModal();
+//     }
+// });
+// });
 
