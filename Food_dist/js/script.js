d926172d70c601932f842f60da44b6757a525346
Ann <annshulyndina@gmail.com>
2020-09-08 12:04:23 +0300

    JSON-server.
    Подключение json. npm

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -305,5 +305,7 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
+    fetch('http://localhost:3000/menu')
+    .then(data => data.json())
+    .then(res => console.log(res));
 });
-
