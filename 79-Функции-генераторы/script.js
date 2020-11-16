0b755ab96ec03692bcd2d30090b97f05eeadd4b0
Ann <annshulyndina@gmail.com>
2020-11-16 15:52:59 +0300

    79-function generation

---
+++ 79-Функции-генераторы/script.js
@@ -0,0 +1,25 @@
+'use strict';
+// function* generator() {
+//     yield 'S';
+//     yield 'D';
+//     yield 'r';
+//     yield 'i';
+//     yield 'p';
+//     yield 't';
+//
+// }
+// const str = generator();
+//
+// console.log(str.next())
+// console.log(str.next())
+
+function* count(n) {
+    for (let i=0; i<n; i++){
+        yield i;
+    }
+}
+const counter = count(7);
+
+console.log(counter.next().value)
+console.log(counter.next().value)
+console.log(counter.next().value)
