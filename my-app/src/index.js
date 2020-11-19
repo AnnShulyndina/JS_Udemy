31c2a28aab16fbd3c66bad19b0b668f022664716
Ann <annshulyndina@gmail.com>
2020-11-19 16:10:27 +0300

    create react components

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,46 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-const Header = () => {
-    const scr = '<script>alert("Error!")</script>';
-    return <h2>{scr}</h2>
-}
+import App from './components/app/';
 
 
-const Field = () => {
-    const holder = 'Enter here';
-    const styledField = {
-        width: '300px',
-        
-    }
-    return <input
-        style = {styledField}
-        type={text}
-        placeholder={holder}
-        autoComplete=""
-        className="first"
-        htmlFor=""/>
-}
-
-const Btn = () => {
-    const text = 'Log in';
-    const logged = true;
-    const res = () => {
-        return ' Log in, please'
-    }
-    return <button>{logged ? "Enter" : text}</button>
-}
-
-const App = () => {
-    return (
-        <div>
-            <Header/>
-            <Field/>
-            <Btn/>
-        </div>
-    )
-}
 ReactDOM.render(
     <React.StrictMode>
         <App/>
