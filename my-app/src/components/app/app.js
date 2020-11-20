81fff0abd7089ee94a4413cc3acff00cb8b76f55
Ann <annshulyndina@gmail.com>
2020-11-20 18:04:15 +0300

    styled-components

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -4,9 +4,16 @@ import SearchPanel from "../search-panel";
 import PostStatusFilter from "../post-status-filter";
 import PostList from "../post-list";
 import PostAddForm from "../post-add-form";
+import styled from 'styled-components'
 
 import './app.css';
 
+const AppBlock = styled.div`
+    margin: 0 auto;
+    max-width: 800px;
+`;
+const StyledAppBlock = styled(AppBlock)`
+    background-color: lightBlue;`
 
 const App = () => {
     
@@ -14,18 +21,22 @@ const App = () => {
         {label: 'Going to learn React', important: true, id: 1},
         {label: 'That is so good', important: false, id: 2},
         {label: 'I need a break...', important: false, id: 3},
-        
     ]
-    return(
-        <div className="app">
+    return (
+        <StyledAppBlock>
+            
             <AppHeader/>
+            
             <div className="search-panel d-flex">
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
+            
             <PostList posts={data}/>
+            
             <PostAddForm/>
-        </div>
+        
+        </StyledAppBlock>
     )
 }
 export default App
