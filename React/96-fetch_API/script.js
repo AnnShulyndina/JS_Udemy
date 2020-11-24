158146f062c794d1e4a19d3ecc2a3105c7fd7f51
Ann <annshulyndina@gmail.com>
2020-11-24 15:55:14 +0300

    API ice and Fire

--- React/96-fetch_API/script.js
+++ React/96-fetch_API/script.js
@@ -1,13 +1,33 @@
-let url = 'https://jsonplaceholder.typicode.com/posts/',
-    data = {username: 'example'}
+class GotService {
     
-fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers:{
-        'Content-Type': 'application/json'
+    constructor() {
+        this._apiBase = 'https://www.anapioficeandfire.com/api';
     }
-})
-    .then((response) => response.json())
-    .then((myJson) => console.log('Success', myJson))
-    .catch(error=>console.error('Error', error))
+    
+    async getResource (url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+        
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+        
+        return await  res.json();
+    };
+    getAllCharacters() {
+        return this.getResource('/characters?page=5&pageSize=10')
+    }
+    getCharacter(id) {
+    return this.getResource('/characters/${id}')
+    }
+    
+}
+
+const got = new GotService();
+got.getAllCharacters()
+    .then(res=>{
+        res.forEach(item=>console.log(item.name))
+    });
+
+got.getCharacter(130)
+    .then(res=>console.log(res))
+
